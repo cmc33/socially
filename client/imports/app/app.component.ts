fd771e65f58820a295ee12b7b9f87175476ba21c
cmc33 <cmcguigan33@gmail.com>
2016-12-08 11:10:44 -0500

    import parties to component ts

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Parties } from '../../../both/collections/parties.collection';
 import template from "./app.component.html";
 import style from "./app.component.scss";
 
