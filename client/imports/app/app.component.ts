c46f9bfd7cba93a9d9645d99ced1fe814b649fd7
cmc33 <cmcguigan33@gmail.com>
2016-12-08 11:14:45 -0500

    bind observable

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Observable } from 'rxjs/Observable';
 import { Parties } from '../../../both/collections/parties.collection';
 import template from "./app.component.html";
 import style from "./app.component.scss";
@@ -8,22 +9,9 @@ import style from "./app.component.scss";
   template
 })
 export class AppComponent {
-  parties: any[];
+  parties: Observable<any[]>;
  
   constructor() {
-    this.parties = [
-      {'name': 'Dubstep-Free Zone',
-        'description': 'Can we please just for an evening not listen to dubstep.',
-        'location': 'Palo Alto'
-      },
-      {'name': 'All dubstep all the time',
-        'description': 'Get it on!',
-        'location': 'Palo Alto'
-      },
-      {'name': 'Savage lounging',
-        'description': 'Leisure suit required. And only fiercest manners.',
-        'location': 'San Francisco'
-      }
-    ];
+    this.parties = Parties.find({}).zone();
   }
 }
