7ec38370e970d8bfc3826927881da8c336116fec
cmc33 <cmcguigan33@gmail.com>
2016-12-08 12:34:47 -0500

    connecting component root view ugghhh

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import template from './app.component.html'; 
  
 @Component({
-  selector: 'app'
+  selector: 'app',
+  template
 })
 export class AppComponent {}
