e5de13b9c0057dd2269f756fc32fb7a294aae3c6
cmc33 <cmcguigan33@gmail.com>
2016-12-12 13:10:45 -0500

    creating multiple views

--- client/imports/app/parties/parties-list.component.ts
+++ client/imports/app/parties/parties-list.component.ts
@@ -1,24 +1,22 @@
 import { Component } from '@angular/core';
-
 import { Observable } from 'rxjs/Observable';
-
-import { Parties } from '../../../both/collections/parties.collection';
-
-import { Party } from '../../../both/models/party.model';
-
-import template from './app.component.html'; 
-
+ 
+import { Parties } from '../../../../both/collections/parties.collection';
+import { Party } from '../../../../both/models/party.model';
+ 
+import template from './parties-list.component.html';
+ 
 @Component({
-  selector: 'app',
+  selector: 'parties-list',
   template
 })
-export class AppComponent {
+export class PartiesListComponent {
   parties: Observable<Party[]>;
  
   constructor() {
     this.parties = Parties.find({}).zone();
   }
-
+ 
   removeParty(party: Party): void {
     Parties.remove(party._id);
   }
