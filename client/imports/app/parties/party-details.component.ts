f5b9a9afb689de490e4abef036160572d31c2032
cmc33 <cmcguigan33@gmail.com>
2016-12-12 19:51:35 -0500

    add links

--- client/imports/app/parties/party-details.component.ts
+++ client/imports/app/parties/party-details.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
  
+import { Subscription } from 'rxjs/Subscription';
+
 import 'rxjs/add/operator/map';
+ 
+import { Parties } from '../../../../both/collections/parties.collection';
+import { Party } from '../../../../both/models/party.model';
  
 import template from './party-details.component.html';
  
@@ -10,16 +15,22 @@ import template from './party-details.component.html';
   template
 })
 
-export class PartyDetailsComponent implements OnInit {
+export class PartyDetailsComponent implements OnInit, OnDestroy {
   partyId: string;
+  paramsSub: Subscription;
+  party: Party;
  
   constructor(
     private route: ActivatedRoute
   ) {}
  
   ngOnInit() {
-    this.route.params
+    this.paramsSub = this.route.params
       .map(params => params['partyId'])
       .subscribe(partyId => this.partyId = partyId);
+  }
+
+  ngOnDestroy() {
+    this.paramsSub.unsubscribe();
   }
 }
