0658fd5624b6475db6ae3aa55e0e16ac4f31134c
cmc33 <cmcguigan33@gmail.com>
2016-12-14 13:59:48 -0500

    improv

--- server/main.ts
+++ server/main.ts
@@ -1,6 +1,8 @@
 import { Meteor } from 'meteor/meteor';
- 
+
 import { loadParties } from './imports/fixtures/parties';
+
+import './imports/publications/parties';
  
 Meteor.startup(() => {
   loadParties();
